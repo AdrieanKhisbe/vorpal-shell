1c3b9ffce694a2303f284f9fb6652f3f8c99feb6
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-03-17 20:51:29 +0100

    test: fix the lint

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ const shelljs = require('shelljs');
 module.exports = (options = {}) => vorpal => {
   const {shell = 'bash'} = options;
 
-  vorpal.command('$ [cmd...]', 'run command in a shell terminal')
+  vorpal
+    .command('$ [cmd...]', 'run command in a shell terminal')
     .autocomplete({
       data: async () => {
         const command = vorpal.ui.input();
-        const shellComp = shelljs.exec(`~/git/vorpal-shell/capture.zsh '${command.substring(2)}'`,
-          {shell: 'zsh', silent: true})
-        return shellComp.stdout.split(/\r?\n/)
-          .map(line => line.split(' ')[0]);
+        const shellComp = shelljs.exec(`~/git/vorpal-shell/capture.zsh '${command.substring(2)}'`, {
+          shell: 'zsh',
+          silent: true
+        });
+        return shellComp.stdout.split(/\r?\n/).map(line => line.split(' ')[0]);
       }
     })
     .allowUnknownOptions()
-    .action(async function (args) {
+    .action(async function() {
+      // note: not using args that is injected by action
       const cmd = vorpal._command.command.substring(2);
       shelljs.exec(cmd, {shell});
-    })
+    });
 };
